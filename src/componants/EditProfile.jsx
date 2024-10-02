bc9fa21eb67271d158a03d22838d2f8255876241
KanhaiyaLalTeli <92908760+KanhaiyaLalTeli@users.noreply.github.com>
2024-10-02 20:42:00 +0530

    Update EditProfile.jsx

--- src/componants/EditProfile.jsx
+++ src/componants/EditProfile.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import UserCard from "./UserCard";
 import axios from "axios";
-import { BASE_URL } from "../utils/Constant";
+import { BASE_URL } from "../utils/constant";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/userSlice";
 
