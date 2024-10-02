703b1b92d84c7ee1cff30c5dd6a691ba9bb7f9b7
KanhaiyaLalTeli <92908760+KanhaiyaLalTeli@users.noreply.github.com>
2024-10-02 20:38:31 +0530

    Update Request.jsx

--- src/componants/Request.jsx
+++ src/componants/Request.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { BASE_URL } from "../utils/Constant";
+import { BASE_URL } from "../utils/constant";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addRequest, removeRequest } from "../utils/requestSlice";
