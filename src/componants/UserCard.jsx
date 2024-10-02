088c37f79f745fe74c2b8518b9af77e2c7f673d2
KanhaiyaLalTeli <92908760+KanhaiyaLalTeli@users.noreply.github.com>
2024-10-02 20:38:49 +0530

    Update UserCard.jsx

--- src/componants/UserCard.jsx
+++ src/componants/UserCard.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { BASE_URL } from "../utils/Constant";
+import { BASE_URL } from "../utils/constant";
 import { useDispatch } from "react-redux";
 import { removeUserFromFeed } from "../utils/feedSlice";
 
